d8d8a45d7f358cfc3dd969c42837e375dd70d674
ayame113 <40050810+ayame113@users.noreply.github.com>
2025-02-24 13:23:03 +0900

    deno typesは不要(2.22)

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,4 @@
-// @deno-types="@types/react"
 import { StrictMode } from "react";
-// @deno-types="@types/react-dom/client"
 import { createRoot } from "react-dom/client";
 import { App } from "./App.tsx";
 
